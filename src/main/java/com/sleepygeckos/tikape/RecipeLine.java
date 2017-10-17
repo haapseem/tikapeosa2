50c1618f70cde007530319fba7e589fbda38c64a
Kesälä Juho M <jkesala@lx7-fuxi259.ad.helsinki.fi>
2017-10-17 14:39:49 +0300

    Add getters and setters to RecipeLine

--- src/main/java/com/sleepygeckos/tikape/RecipeLine.java
+++ src/main/java/com/sleepygeckos/tikape/RecipeLine.java
@@ -17,4 +17,36 @@ public class RecipeLine {
         this.amount = amount;
         this.text = text;
     }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
