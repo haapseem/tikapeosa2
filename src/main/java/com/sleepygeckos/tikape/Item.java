c592f081ad1231c32e23f03b9cd128851c1dcc8a
Kesälä Juho M <jkesala@lx7-fuxi259.ad.helsinki.fi>
2017-10-17 15:53:00 +0300

    sdf

--- src/main/java/com/sleepygeckos/tikape/Item.java
+++ src/main/java/com/sleepygeckos/tikape/Item.java
@@ -25,6 +25,30 @@ public abstract class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Item other = (Item) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
     
     
 }
