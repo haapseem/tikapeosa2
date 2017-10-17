dbf123ec620fd1cf43dbbf1d60de8b088da49ddf
Kesälä Juho M <jkesala@lx7-fuxi259.ad.helsinki.fi>
2017-10-17 15:49:10 +0300

    Add RecipeLine-related methods to Food

--- src/main/java/com/sleepygeckos/tikape/Food.java
+++ src/main/java/com/sleepygeckos/tikape/Food.java
@@ -1,15 +1,26 @@
-
 package com.sleepygeckos.tikape;
 
 import java.util.List;
 
 public class Food extends Item {
-    
+
     List<RecipeLine> recipeLines;
-    
+
     public Food(int id, String name) {
         super(id, name);
     }
-    
-    
+
+    public void addRecipeLine(RecipeLine recipeLine) {
+        this.recipeLines.add(recipeLine);
+    }
+
+    public void addRecipeLines(List<RecipeLine> recipeLinesToAdd) {
+        for (RecipeLine recipeLine : recipeLinesToAdd) {
+            this.recipeLines.add(recipeLine);
+        }
+    }
+
+    public void ClearRecipeLines() {
+        this.recipeLines.clear();
+    }
 }
