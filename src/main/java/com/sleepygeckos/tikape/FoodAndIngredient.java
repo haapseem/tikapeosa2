b9225ffe60ffdbe7f28446a3907a0e6c814b38df
Olli2 <olli.eskola@outlook.com>
2017-10-17 12:58:54 +0300

    Created super class FoodAndIngredient which extends Item

---
+++ src/main/java/com/sleepygeckos/tikape/FoodAndIngredient.java
@@ -0,0 +1,11 @@
+
+package com.sleepygeckos.tikape;
+
+public class FoodAndIngredient extends Item {
+    
+    public FoodAndIngredient(int id, String name) {
+        super(id, name);
+    }
+    
+    
+}
