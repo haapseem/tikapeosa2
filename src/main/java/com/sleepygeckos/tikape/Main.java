226898a84291c7df3c195c680ba4128e2a0bb35e
Eemil Haapsaari <eemil@haapsaari.fi>
2017-10-17 12:43:02 +0300

    new html files

--- src/main/java/com/sleepygeckos/tikape/Main.java
+++ src/main/java/com/sleepygeckos/tikape/Main.java
@@ -1,14 +1,18 @@
 package com.sleepygeckos.tikape;
 
+import java.util.HashMap;
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Spark.get("/hei", (req, res) -> {
-            return "Hei maailma!!";
-        });
+        Spark.get("/", (req, res) -> {
+            HashMap map = new HashMap<>();
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
 
     }
 
