a8b684fe1c1617e56ca3b70b817bf991c21d447e
Olli2 <olli.eskola@outlook.com>
2017-10-19 21:24:20 +0300

    Made get methods to Main and some fixes to index

--- src/main/java/com/sleepygeckos/tikape/Main.java
+++ src/main/java/com/sleepygeckos/tikape/Main.java
@@ -2,6 +2,7 @@ package com.sleepygeckos.tikape;
 
 import com.sleepygeckos.tikape.database.DbHandler;
 import com.sleepygeckos.tikape.handlers.FoodHandler;
+import com.sleepygeckos.tikape.handlers.IngredientHandler;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,22 +15,27 @@ public class Main {
     public static void main(String[] args) throws SQLException, Exception {
         DbHandler dbhandler = new DbHandler();
         FoodHandler foodHandler = new FoodHandler(dbhandler);
-        
+        IngredientHandler ingredientHandler = new IngredientHandler(dbhandler);
+
         Spark.get("/", (req, res) -> {
-            HashMap map = new HashMap<>();  
+            HashMap map = new HashMap<>();
             map.put("foods", foodHandler.getItems());
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
-        
-        Spark.get("/fdsa", (req, res) -> {
-            HashMap map = new HashMap<>();  
-            map.put("foods", foodHandler.getItems());
-            return new ModelAndView(map, "ingredients");
+
+        Spark.get("/ingredients", (req, res) -> {
+            System.out.println("asd");
+            HashMap ingredientMap = new HashMap<>();
+            ingredientMap.put("ingredients", ingredientHandler.getItems());
+            return new ModelAndView(ingredientMap, "ingredients");
         }, new ThymeleafTemplateEngine());
-        
-        
-        
-        
+
+        Spark.get("/createsmoothie", (req, res) -> {
+            HashMap foodMap = new HashMap<>();
+            foodMap.put("foods", foodHandler.getItems());
+            return new ModelAndView(foodMap, "createsmoothie");
+        }, new ThymeleafTemplateEngine());
+
     }
 
 }
